071bfda2c07799a98d8c14a3b9ac0a021f08fdaa
Francois Bisschop <francois.bisschop@hva.nl>
2018-01-25 22:14:56 +0100

    fetching db json arrays still not working

--- alexa-react/src/views/Dashboard.js
+++ alexa-react/src/views/Dashboard.js
@@ -5,28 +5,34 @@ export default class Dashboard extends React.Component {
   state = {tests: []};
 
   componentDidMount() {
+    //fetch('/test')
+    // .then(res => res.json())
+    // .then(tests => this.setState({tests: []}))
+    //
     fetch('/test')
-      .then(res => res.json())
-      .then(tests => this.setState({ tests : [] }));
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(json) {
+        json => this.setState({test : json.name});
+      })
   }
 
   render() {
     return (
       <div>
-        <Grid container spacing={24}>
-          <Grid item xs={12}>
-            <h1>Dashboard</h1>
-            <p>
-              Hier staat alle informatie over de gegeven commando's en maakt het
-              mogelijk om zelf dingen in te voeren
-            </p>
-          </Grid>
-            {this.state.tests.map(test =>
-              <div key={test.name}>{test.year}</div>
-            )}
-        </Grid>
+        <h1>Dashboard</h1>
+        <p>
+          Hier staat alle informatie over de gegeven commando's en maakt het
+          mogelijk om zelf dingen in te voeren
+        </p>
+        {this.state.tests}
+        {this.state.tests.map(test => (
+          <div key={test._id}>
+            {test.name} {test.year}
+          </div>
+        ))}
       </div>
     );
   }
 }
-
